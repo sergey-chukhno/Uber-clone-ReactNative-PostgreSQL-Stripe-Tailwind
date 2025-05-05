56193c4784dc14b8ca76f0bfd4755c6cdc36f465
Sergey Chukhno <sergey.chukhno@laplateforme.io>
2025-05-05 16:18:20 +0200

    Created project structure, installed components and dependencies

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home page</Text>
+    </SafeAreaView>
+  )
+ };
+
+export default Home;
