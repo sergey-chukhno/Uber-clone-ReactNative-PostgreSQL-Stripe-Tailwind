264770fa7738f7f8ec14552a58ec17ccbc4458eb
Sergey Chukhno <sergey.chukhno@laplateforme.io>
2025-05-17 17:31:47 +0200

    🔥 Onboarding section completed
    Completed succesfully onboarding screens of the application

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -9,6 +9,7 @@ import CustomButton from "@/components/CustomButton";
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
   const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -51,7 +52,15 @@ const Onboarding = () => {
         ))}
       </Swiper>
 
-      <CustomButton title="Next" className="w-11/12 mt-10" />
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("./(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10"
+      />
     </SafeAreaView>
   );
 };
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -47,11 +47,13 @@ const CustomButton = ({
   className,
   IconLeft,
   IconRight,
+  ...props
 }: CustomButtonProps) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      className={`w-full rounded-full flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+      className={`w-full rounded-full p-3 flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+      {...props}
     >
       {IconLeft && <IconLeft />}
       <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
